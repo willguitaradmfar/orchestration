1cc4dca07457fbdd110cdc500d4ce6fd488c2291
Your Name <willguitaradmfar@gmail.com>
2016-02-09 19:16:43 -0200

    https://github.com/willguitaradmfar/orchestration.git

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,6 +8,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var http = require('http');
+var session = require('express-session');
 
 var Orquestration = require('./orchestration');
 
@@ -14,7 +17,7 @@ _global = new Orquestration();
 var app = express();
 var server = http.createServer(app);
 
-var pathModules = process.env.MODULES || '/home/will/git/orchestration/modules/';
+var pathModules = process.env.MODULES || process.env.HOME + '/modules/';
 
 app.set('port', process.env.PORT || 3000);
 
@@ -23,6 +26,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(session({secret: 'session'}));
 
 if (!fs.existsSync(pathModules)) {
     fs.mkdirSync(pathModules);
@@ -33,25 +37,51 @@ var modules = fs.readdirSync(pathModules);
 if (modules.length == 0) {
     console.log('Não existe modulos. Para adicionar:');
     console.log('1)\t', '(sudo npm install express-generator -g)');
-    console.log('2)\t', '(express modules/mymodule)');
+    console.log('2)\t', '(express ' + pathModules + '/mymodule)');
     console.log('3)\t', '(http://localhost:3000/mymodule)');
 }
 
 for (var i in modules) {
     var module = modules[i];
+    console.log('Importing module', '(' + module + ')', '...');    
+    registerView(module);    
+}
+
+for (var i in modules) {
+    var module = modules[i];
     console.log('Importing module', '(' + module + ')', '...');
-    register(module);
+    registerRule(module);    
+}
+
+for (var i in modules) {
+    var module = modules[i];
+    console.log('Importing module', '(' + module + ')', '...');    
+    registerCSS(module);    
+}
+
+for (var i in modules) {
+    var module = modules[i];
+    console.log('Importing module', '(' + module + ')', '...');    
+    registerJS(module);
+}
+
+for (var i in modules) {
+    var module = modules[i];
+    console.log('Importing module', '(' + module + ')', '...');    
+    registerRouter(module);    
 }
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('Não encontrado. Verifique o modulo');
     err.status = 404;
     next(err);
 });
 
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
+        if(err.status != 404)
+            console.trace(err);
         res.status(err.status || 500);
         res.json({
             message: err.message,
@@ -61,6 +91,8 @@ if (app.get('env') === 'development') {
 }
 
 app.use(function (err, req, res, next) {
+    if(err.status != 404)
+        console.trace(err);
     res.status(err.status || 500);
     res.json({
         message: err.message,
@@ -71,12 +103,7 @@ app.use(function (err, req, res, next) {
 if (modules.length > 0)
     server.listen(app.get('port'));
 
-
-function register(moduleName) {
-    
-    var jss = fs.readdirSync(pathModules + moduleName + '/public/javascripts/');
-
-    var routes = fs.readdirSync(pathModules + moduleName + '/routes/');
+function registerRule(moduleName) {
 
     if (!fs.existsSync(pathModules + moduleName + '/rules/')) {
         fs.mkdirSync(pathModules + moduleName + '/rules/');
@@ -85,28 +112,84 @@ function register(moduleName) {
 
     var _path = pathModules + moduleName;
     
-    for (var i in jss) {
-        var jsName = jss[i];        
-        _global.addJS(moduleName , jsName, moduleName + '/javascripts/' + jsName);
-        console.log('\t', 'jss:  ', _path + '/public/javascripts/' + jsName);
-    }
+    console.log('\t', 'rule');
 
-    for (var i in rules) {
+    for (var i in rules) {        
         var ruleName = rules[i];
+        console.log('\t\t', 'rules:  ', _path + '/routes/' + ruleName);
         var _requireRule = require(_path + '/rules/' + ruleName);
-        _global.atach(moduleName, ruleName, _requireRule);
-        console.log('\t', 'rules:  ', _path + '/routes/' + ruleName);
-    }
+        _global.atach(moduleName, ruleName, _requireRule);        
+    }   
+    
+    console.log('---------------------------------------------------------------------------------');
+};
+
+function registerRouter(moduleName) {
+
+    var routes = fs.readdirSync(pathModules + moduleName + '/routes/');
+    
+    var _path = pathModules + moduleName;
+    
+    console.log('\t', 'router');
 
     for (var i in routes) {
         var routeName = routes[i];
+        console.log('\t\t', 'rotas:  ', _path + '/routes/' + routeName);
         var _requireRouter = require(_path + '/routes/' + routeName);
         app.set('views', path.join(_path, 'views'));
-        app.use('/' + moduleName, express.static(_path + '/public/'));
         app.use('/' + moduleName, _requireRouter);
-        console.log('\t', 'rotas:  ', _path + '/routes/' + routeName);
+        app.use('/' + moduleName, express.static(_path + '/public/'));        
     }
-    console.log('\t', 'public: ', _path + '/public/');
-    console.log('\t', 'views:  ', path.join(_path, 'views'));
+    console.log('\t\t', 'public: ', _path + '/public/');
+    console.log('\t\t', 'views:  ', path.join(_path, 'views'));
+    console.log('---------------------------------------------------------------------------------');
+};
+
+function registerCSS(moduleName) {
+    
+    var csss = fs.readdirSync(pathModules + moduleName + '/public/stylesheets/');
+    
+    var _path = pathModules + moduleName;
+    
+    console.log('\t', 'css');
+    
+    for (var i in csss) {
+        var cssName = csss[i];
+        console.log('\t\t', 'csss:  ', _path + '/public/stylesheets/' + cssName);
+        _global.addCSS(moduleName , cssName, '/' + moduleName + '/stylesheets/' + cssName);        
+    }   
+    
+    console.log('---------------------------------------------------------------------------------');
+};
+
+function registerJS(moduleName) {
+    
+    var jss = fs.readdirSync(pathModules + moduleName + '/public/javascripts/');    
+    
+    var _path = pathModules + moduleName;
+    
+    console.log('\t', 'jss');
+    
+    for (var i in jss) {
+        var jsName = jss[i];
+        console.log('\t\t', 'jss:  ', _path + '/public/javascripts/' + jsName);        
+        _global.addJS(moduleName , jsName, '/' +moduleName + '/javascripts/' + jsName);
+    }
+    
+    console.log('---------------------------------------------------------------------------------');
+};
+
+function registerView(moduleName) {
+    
+    var views = fs.readdirSync(pathModules + moduleName + '/views/');    
+    
+    var _path = pathModules + moduleName;
+    
+    console.log('\t', 'views');
+    
+    console.log('\t\t', 'views:  ', _path + '/views/');
+    _global.addView(moduleName, _path + '/views/');
+    
+    
     console.log('---------------------------------------------------------------------------------');
 };
--- src/orchestration.js
+++ src/orchestration.js
@@ -1,24 +1,60 @@
 
-module.exports = function (){
+module.exports = function () {
     this.modules = {};
     this.jss = {};
+    this.csss = {};
+    this.views = {};
 };
 
-module.exports.prototype.require = function (module, name){
+module.exports.prototype.require = function (module, name) {
+    if (!this.modules[module]) {
+        console.trace('Modulo (' + module + ') não existe');
+        throw 'Modulo (' + module + ') não existe';
+    }
+    if (!this.modules[module][name]) {
+        console.trace('Rule (' + name + ') não existe');
+        throw 'Rule (' + name + ') não existe';
+    }
     return this.modules[module][name];
 };
 
-module.exports.prototype.atach = function (module, name, obj){
-    this.modules[module] = {};
-    this.modules[module][name] = obj;    
+module.exports.prototype.atach = function (module, name, obj) {
+    if (!this.modules[module])
+        this.modules[module] = {};
+    this.modules[module][name] = obj;
 };
 
-module.exports.prototype.addJS = function (module, name, JS){
-    this.jss[module] = {};
+module.exports.prototype.addJS = function (module, name, JS) {
+    if (!this.jss[module])
+        this.jss[module] = {};
     this.jss[module][name] = JS;
 };
 
-module.exports.prototype.getJS = function (module, name, JS){
-    return this.jss[module][name];
+module.exports.prototype.getJS = function (module) {
+    if (!this.jss[module])
+        throw 'Modulo ' + module + ' não existe para JS';
+    return this.jss[module];
+};
+
+module.exports.prototype.addView = function (module, view) {
+    this.views[module] = view;
+};
+
+module.exports.prototype.getView = function (module) {
+    if (!this.views[module])
+        throw 'Modulo ' + module + ' não existe para View ';
+    return this.views[module];
+};
+
+module.exports.prototype.addCSS = function (module, name, CSS) {
+    if (!this.csss[module])
+        this.csss[module] = {};
+    this.csss[module][name] = CSS;
+};
+
+module.exports.prototype.getCSS = function (module) {
+    if (!this.csss[module])
+        throw 'Modulo ' + module + ' não existe para CSS ';
+    return this.csss[module];
 };
 
